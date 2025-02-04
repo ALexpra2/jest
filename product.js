46dbe73e0bfdd5d1897c0e22501cd0eba6788402
Alexpra2 <alexpra2@gmail.com>
2025-02-04 22:00:02 +0100

    correcciones clase

--- product.js
+++ product.js
@@ -12,6 +12,10 @@ function addProduct(name, price) {
     }
     if (products.some(product => product.name === name)) {
         throw new Error('Product already exists');
+
+        //Se puede poner como  
+        // if (products.some(product => product.name === name)) throw new Error('Product already exists');
+        //en una sola linea
     }
     // Crear el producto y añadirlo al array products
     id = products.length +1;
@@ -22,15 +26,18 @@ function addProduct(name, price) {
     };
 
     products.push(product);
+    //lo ponemos para no llamar a la funcion en el test
+    return products
 }
 
-function removeProduct(productId) {
-    const index = products.findIndex(product => product.id === productId);
+function removeProduct(id) {
+    const index = products.findIndex(product => product.id === id);
     //findIndex develve el indice o -1 si no ls encuentra
     if (index === -1) {
-        throw new Error('Product does not exist');
+        throw new Error('Product does not exists');
     }
     products.splice(index, 1);
+    //tambien podemos hacer   products = products.find(product => product.id !== id);
 }
 
 function getProducts() {
@@ -40,7 +47,7 @@ function getProducts() {
 function getProduct(productId) {
     const product = products.find(product => product.id === productId);
     if (!product) {
-        throw new Error('Product does not exist');
+        throw new Error('Product does not exists');
     }
     return product;
 }
@@ -48,7 +55,7 @@ function getProduct(productId) {
 function updateProduct(productId, name, price) {
     const product = products.find(product => product.id === productId);
     if (!product) {
-        throw new Error('Product does not exist');
+        throw new Error('Product does not exists');
     }
     if (name) product.name = name;
     if (price) product.price = price;
--- product.test.js
+++ product.test.js
@@ -4,12 +4,20 @@ beforeEach(() => {
     resetProducts();
 });
 
+//! ADDProduct
+
 describe('Adding Products', () => {
     it('should add a product', () => {
         addProduct('apple', 2);
         expect(getProducts()).toContainEqual({ id: 1, name: 'apple', price: 2 });
     });
-
+    
+    //si pongo return en la función no me hace falta lanzar addproduct antes
+    it('should add a product', () => {
+        expect(addProduct('apple', 2)).toContainEqual({ id: 1, name: 'apple', price: 2 });
+    });
+    
+    //si el nombre ya existe no me deja crear el producto
     it('should fail when adding a repeated product', () => {
         addProduct('apple', 2);
         expect(() => addProduct('apple', 2)).toThrow('Product already exists');
@@ -28,17 +36,28 @@ beforeEach(() => {
     resetProducts();
 });
 
+//! renoveProduct
+
+
 describe('Removing Products', () => {
+    //test producto eliminado correctamente
     it('should remove a product', () => {
         addProduct('apple', 2);
         removeProduct(1);
         expect(getProducts()).toEqual([]);
     });
+
+    //Intentar eliminar un producto que no existe
+    it('Should throw an error if the product does not exists', () =>{
+        expect(() => removeProduct(99)).toThrow('Product does not exist')
+    })
 });
 
 beforeEach(() => {
     resetProducts();
 });
+
+//! getProduct
 
 describe('Getting a single product', () => {
     it('should get a product', () => {
@@ -51,15 +70,19 @@ beforeEach(() => {
     resetProducts();
 });
 
+//! updateProduct
+
 describe('Updating Products', () => {
+
+    //test producto actualizado correctamente
     it('should update a product', () => {
         addProduct('apple', 2);
         updateProduct(1, 'banana', 3);
         expect(getProduct(1)).toEqual({ id: 1, name: 'banana', price: 3});
     });
 
-    it('should fail when updating a product that does not exist', () => {
-        expect(() => updateProduct(2, 'pear', 4)).toThrow('Product does not exist');
+    it('should fail when updating a product that does not exists', () => {
+        expect(() => updateProduct(2, 'pear', 4)).toThrow('Product does not exists');
     });
 
     it('should only update the price', () => {
